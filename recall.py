eb51e09d761624a8f58bd9c8d211b7fea65a4219
Diogo Neves <diogo.neves@gmail.com>
2024-06-08 16:43:53 +0100

    created a simple open ai sample app

---
+++ recall.py
@@ -0,0 +1,16 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.environ.get("STREAM_OPEN_AI_KEY")
+)
+
+completion = client.chat.completions.create(
+  model="gpt-4o",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message.content)
