1aae959d5322f1de51e14c13db2bac6f26963db3
Diogo Neves <diogo.neves@gmail.com>
2024-06-11 18:40:44 +0100

    started implementing store and recall

--- recall.py
+++ recall.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import base64
+from openai import OpenAI
 
 # 1. Assume screenshots exist
 # 2. Extract details from the screenshot
@@ -9,7 +10,8 @@ import base64
 
 DATA_FOLDER = "data/"
 
-MODEL_NAME = "gpt-4o"
+EMBEDDING_MODEL_NAME = "text-embedding-3-small"
+INFERENCE_MODEL_NAME = "gpt-4o"
 API_KEY = os.environ.get("STREAM_OPEN_AI_KEY")
 OPEN_AI_HEADERS = {
   "Content-Type": "application/json",
@@ -27,12 +29,15 @@ SYSTEM_PROMPT = ("You are an assistant looking through the user's macOS screen"
                  " remember what they were doing in the past.")
 
 
-def extract_details_from_screenshot(image_path):
+openai_client = OpenAI(api_key=API_KEY)
+
+
+def extract_details_from_screenshot(image_path: str) -> str:
   base64_image = _encode_image(image_path)
   user_message = ("Provide a short description of what's happening in this"
                   " screenshot.")
   payload = {
-    "model": MODEL_NAME,
+    "model": INFERENCE_MODEL_NAME,
     "messages": [
       {
           "role": "system",
@@ -64,12 +69,39 @@ def extract_details_from_screenshot(image_path):
   return model_response["choices"][0]["message"]["content"]
 
 
-def _encode_image(image_path):
+def _encode_image(image_path: str) -> str:
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+Embedding = list[float]
+
+details_store: list[str] = []
+vectors_store: list[Embedding] = []
+
+
+def store_details(details: str) -> None:
+  embedding = _vectorise_text(details)
+
+  details_store.append(details)
+  vectors_store.append(embedding)
+
+
+def _vectorise_text(text: str) -> Embedding:
+  response = openai_client.embeddings.create(
+      input=text,
+      model=EMBEDDING_MODEL_NAME
+  )
+  return response.data[0].embedding
+
+
+def recall(query: str) -> list[str]:
+  query_embedding = _vectorise_text(query)
+  return []
 
 
 if __name__ == "__main__":
   image_path = DATA_FOLDER + "2024-06-08_16.27.41.png"
   details = extract_details_from_screenshot(image_path)
   print(details)
+  store_details(details)
